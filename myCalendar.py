6ab61f765dc54ee4709e30af58e7fa86af8c97f4
DennisB1109 <112759597+DennisB1109@users.noreply.github.com>
2022-09-03 15:12:20 +0200

    Added some functions
    Implemented functions that print either the current day, the current month, the current yeah or the whole current date

--- myCalendar.py
+++ myCalendar.py
@@ -1,1 +1,20 @@
-print("LETS GET STARTED")
+import datetime
+
+print("LETS GET STARTED\n")
+
+def getCurrDay():
+    convert_to_german = {"Monday" : "Montag", "Tuesday" : "Dienstag", "Wednesday" : "Mittwoch", "Thursday" : "Donnerstag", "Friday": "Freitag", "Saturday" : "Samstag", "Sunday" : "Sonntag"}
+    weekday = datetime.datetime.now().strftime("%A")
+    print("Heute ist der %ste, das ist ein" % (datetime.datetime.now().day), (convert_to_german.get(weekday)))
+
+def getCurrMonth():
+    month_name = {1 : "Januar", 2: "Februar", 3: "März", 4: "April", 5: "Mai", 6: "Juni", 7: "Juli", 8: "August", 9: "September", 10: "Oktober", 11: "November", 12: "Dezember"}
+    number_of_month = datetime.datetime.now().month
+    print("Wir haben", month_name.get(number_of_month), "(%s)" % (number_of_month))
+    
+
+def getCurrYear():
+    print("Wir haben das Jahr:", datetime.datetime.now().year)
+
+def getDate():
+    print("Heutiges datum: %s-%s-%s" % (datetime.datetime.now().day, datetime.datetime.now().month, datetime.datetime.now().year))
