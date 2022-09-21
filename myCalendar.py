e4702bef5717cb3571fd28ace31fe93163e1075b
DennisB1109 <112759597+DennisB1109@users.noreply.github.com>
2022-09-21 11:26:30 +0200

    Update myCalendar.py

--- myCalendar.py
+++ myCalendar.py
@@ -3,21 +3,29 @@ import datetime
 print("LETS GET STARTED\n")
 
 def get_CurrDay():
+    """_summary_
+    """
     convert_to_german = {"Monday" : "Montag", "Tuesday" : "Dienstag", "Wednesday" : "Mittwoch", "Thursday" : "Donnerstag", "Friday": "Freitag", "Saturday" : "Samstag", "Sunday" : "Sonntag"}
     weekday = datetime.datetime.now().strftime("%A")
     print("Heute ist der %ste, das ist ein" % (datetime.datetime.now().day), (convert_to_german.get(weekday)))
+    return weekday
 
 def get_CurrMonth():
+    """_summary_
+    """
     month_name = {1 : "Januar", 2: "Februar", 3: "März", 4: "April", 5: "Mai", 6: "Juni", 7: "Juli", 8: "August", 9: "September", 10: "Oktober", 11: "November", 12: "Dezember"}
     number_of_month = datetime.datetime.now().month
     print("Wir haben", month_name.get(number_of_month), "(%s)" % (number_of_month))
+    return number_of_month
     
 
 def get_CurrYear():
     print("Wir haben das Jahr:", datetime.datetime.now().year)
+    return datetime.datetime.now().year
 
 def get_Date():
     print("Heutiges datum: %s-%s-%s" % (datetime.datetime.now().day, datetime.datetime.now().month, datetime.datetime.now().year))
+    return datetime.datetime.now().day, datetime.datetime.now().month, datetime.datetime.now().year
 
 def add_Event(): 
     with open("my_Events.xls", "a", encoding="utf-8") as _:
@@ -28,5 +36,3 @@ def add_Event():
         appointment_day = _.write(day)
         appointment_month = _.write(month)
         appointment_year = _.write(year)
-
-add_Event()
